2ded8b906004a548adea05ebb27c71be2767f2bf
Developer <daravojka505@gmail.com>
2026-05-09 17:47:08 +0300

    feat(3): Complete Book CRUD operations

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,60 @@
+class BooksController < ApplicationController
+  before_action :require_login, except: [ :index, :show ]
+  before_action :set_book, only: [ :show, :edit, :update, :destroy ]
+  before_action :check_ownership, only: [ :edit, :update, :destroy ]
+
+  def index
+    @books = if params[:search].present?
+               Book.where("title ILIKE ? OR description ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    else
+               Book.all
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to @book, notice: "Book was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to @book, notice: "Book was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    title = @book.title
+    @book.destroy
+    redirect_to books_url, notice: "Book '#{title}' was successfully deleted."
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def check_ownership
+    redirect_to books_path, alert: "Not authorized" unless @book.user == current_user
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :description, :isbn, :published_year, :author_id)
+  end
+end
